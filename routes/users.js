e66b3c56d3060ee46677dd0274d70960079d27f5
hejiale <823818902@qq.com>
2026-08-08 16:37:26 +0800

    first commit

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ function createUsersRouter(pool) {
   // 查询用户列表
   router.get('/', async (req, res) => {
     try {
-      const [rows] = await pool.query('SELECT * FROM users ORDER BY id DESC');
+      const [rows] = await pool.query('SELECT * FROM users');
       res.json({ code: 200, data: rows });
     } catch (err) {
       res.status(500).json({ code: 500, message: err.message });
